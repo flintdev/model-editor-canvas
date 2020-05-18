d50a99f77571b17893f1f0872351e063eceb768a
cc189 <cc189dev@gmail.com>
2020-05-17 23:43:24 -0700

    add OperationsInterface

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,28 @@
 import {ModelEditorCanvas} from "./components/ModelEditorCanvas"
 
+interface OperationsInterface {
+    getUUID?: () => string,
+    updateBlockData?: (data: BlockData) => any
+}
+
+interface BlockItem {
+    id: string,
+    name: string,
+    dataType: "string" | "integer" | "object" | "boolean" | "string[]" | "integer[]" | "$ref" | "$ref[]",
+    required?: boolean,
+}
+
+interface BlockData {
+    blockId: string,
+    name: string,
+    items: BlockItem[],
+    refs: {[k: string]: string}
+}
+
+
 export {
+    OperationsInterface,
+    BlockItem,
+    BlockData,
     ModelEditorCanvas
 }
