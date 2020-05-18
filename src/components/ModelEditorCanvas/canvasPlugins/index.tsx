9218976e170e9ee1362d401f2eb5c98e857893bc
cc189 <cc189dev@gmail.com>
2020-05-17 23:43:48 -0700

    update PLUGIN_EVENTS

--- src/components/ModelEditorCanvas/canvasPlugins/index.tsx
+++ src/components/ModelEditorCanvas/canvasPlugins/index.tsx
@@ -2,8 +2,11 @@ import {ENGINE_MODE} from "../../../flint-react-canvas";
 
 export const PLUGIN_EVENTS = {
     PLUGIN_NODE_CREATE: "pluginnode::create",
-    PLUGIN_CONNECTION_CREATE: "pluginconnection::create",
-    NODE_DOUBLECLICK: "node::doubleclick"
+    PLUGIN_CHAIN_CREATE: "pluginchain::create",
+    NODE_DOUBLECLICK: "node::doubleclick",
+    NODE_UPDATE_BLOCKNAME: "node:updateblockname",
+    PLUGIN_CHAIN_RERENDER: "pluginchain::rerender",
+    PLUGIN_CHAIN_CANCEL: "pluginchain::cancel",
 };
 
 export const PLUGIN_ENGINE_MODE = {
